2daf9f9fac4e7416c03207c92a1499c68557aaea
Darío Javier Cravero <dario@uxtemple.com>
2017-02-14 17:29:25 +0000

    fix: publish es5

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,5 @@
+import buble from 'rollup-plugin-buble';
+
 const pkg = require('./package.json');
 const external = Object.keys(pkg.devDependencies).concat(Object.keys(pkg.dependencies));
 
@@ -6,4 +8,5 @@ export default {
   entry: 'index.js',
   format: 'cjs',
   external,
+  plugins: process.env.NODE_ENV === 'production' ? [buble()] : undefined,
 };
